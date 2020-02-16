dd193a277c070cf60c49f8a5528b0998c7b4dca6
ren_mcc <ren_mcc@foxmail.com>
2020-02-16 23:57:55 +0800

    初始化

---
+++ mysite/blog/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+#__time__: 2020/2/16 0:47
+#__author__ = 'ren_mcc'
+
+from django.conf.urls import url,include
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.blog_title, name='blog_title'),
+    url(r'(?P<article_id>\d)/$', views.blog_article,name="blog_detail"),
+    url(r'hello/', views.hello,name="hello"),
+]
